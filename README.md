dcb5288072269e179253a5902e2fda3d7f4ad887
Josiah Eakle <josiaheakle@gmail.com>
2021-12-07 00:10:12 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Easy DSF
+
+A platform to easily manage your digital storefronts to set you up for success!
---
+++ src/comps/pages/Homepage.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+interface HomepageProps {}
+
+const Homepage: React.FC<HomepageProps> = ({}) => {
+	return <h1>Hello world</h1>;
+};
+
+export { Homepage };
